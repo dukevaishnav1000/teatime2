b9491f4f67da4347dfb539d16f96ab82d1096f28
dukevaishnav1000 <dukevaishnav3853@gmail.com>
2023-01-15 23:43:53 +0530

    rectifying code

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -7,20 +7,20 @@ class Navbar extends React.Component{
     render(){
         return(<nav id="navbarcomp" className="navbar navbar-expand-lg navbar-light bg-light nav-fill">
             <div className="container-fluid ">
-              <a className="navbar-brand " href="/"><h1 className="text-primary">Tea<span className="text-success">Time</span><img id="teaimg" src="https://www.freepnglogos.com/uploads/tea-png/tea-top-afternoon-teas-around-the-red-letter-days-blog-8.png"/></h1></a>
+              <Link to="/"><a className="navbar-brand"><h1 className="text-primary">Tea<span className="text-success">Time</span><img id="teaimg" src="https://www.freepnglogos.com/uploads/tea-png/tea-top-afternoon-teas-around-the-red-letter-days-blog-8.png"/></h1></a></Link>
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
               <div className="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <li className="nav-item mt-2">
-                    <Link to="/Technology"><button type="button" className="btn btn-primary mr-2 ml-2">Technology</button></Link>
+                    <li className="nav-item mt-2 mr-2 ml-2">
+                    <Link to="/Technology"><button type="button" className="btn btn-primary ">Technology</button></Link>
                     </li>
-                    <li className="nav-item mt-2">
-                        <Link to="/Entertainment"><button type="button" className="btn btn-primary mr-2 ml-2">Entertainment</button></Link>
+                    <li className="nav-item mt-2 mr-2 ml-2">
+                        <Link to="/Entertainment"><button type="button" className="btn btn-primary ">Entertainment</button></Link>
                     </li>
-                    <li className="nav-item mt-2">
-                    <Link to="/Sports"><button type="button" className="btn btn-primary mr-2 ml-2">Sports</button></Link>
+                    <li className="nav-item mt-2 mr-2 ml-2">
+                    <Link to="/Sports"><button type="button" className="btn btn-primary">Sports</button></Link>
                     </li>
                     <li className="nav-item mt-2">
                     <Link to="/Politics"><button type="button" className="btn btn-primary mr-2 ml-2">Politics</button></Link>
